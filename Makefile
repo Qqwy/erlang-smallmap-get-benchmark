c6998f82588e2e6618cfae15b72d1c0f334587e7
Marten/Qqwy <w-m@resilia.nl>
2022-07-14 17:19:58 +0200

    A makefile for reproducible builds

---
+++ Makefile
@@ -0,0 +1,26 @@
+gccO3: *.c *.cpp
+	gcc -c -fPIC -O3 -g -Wall -Werror implementations.c
+	g++ -c -fPIC -O3 -g -Wall -Werror main.cpp
+	g++ -o gccO3 implementations.o main.o
+	rm *.o
+
+gccO2: *.c *.cpp
+	gcc -c -fPIC -O2 -g -Wall -Werror implementations.c
+	g++ -c -fPIC -O2 -g -Wall -Werror main.cpp
+	g++ -o gccO2 implementations.o main.o
+	rm *.o
+
+clangO3: *.c *.cpp
+	clang -c -fPIC -O3 -g -Wall -Werror implementations.c
+	clang++ -c -fPIC -O3 -g -Wall -Werror main.cpp
+	clang++ -o clangO3 implementations.o main.o
+	rm *.o
+
+clangO2: *.c *.cpp
+	clang -c -fPIC -O2 -g -Wall -Werror implementations.c
+	clang++ -c -fPIC -O2 -g -Wall -Werror main.cpp
+	clang++ -o clangO2 implementations.o main.o
+	rm *.o
+
+clean:
+	-rm *.o gccO3 gccO2 clangO3 clangO2
--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 
 using function_under_test = Uint *(Uint *, Uint *, Uint, Uint);
 
-#define foo_ptr ()
-
+extern "C" {
 Uint *original_flatmap_get(Uint *keys, Uint *vals, Uint key, Uint max_size);
 Uint *latereturn_flatmap_get(Uint *keys, Uint *vals, Uint key, Uint max_size);
 Uint *vectoradd_flatmap_get(Uint *keys, Uint *vals, Uint key, Uint max_size);
 Uint *vectoror_flatmap_get(Uint *keys, Uint *vals, Uint key, Uint max_size);
+}
 
 static Uint keys[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
                       11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -24,7 +24,8 @@ static Uint vals[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
 template <function_under_test FUT> static void b(picobench::state &s) {
   std::random_device rd{};
   auto gen = std::mt19937{rd()};
-  auto distr = std::uniform_int_distribution<>(0, 31);
+  auto distr = std::uniform_int_distribution<>(
+      0, s.iterations() * 2 - 1); // 50% chance of missing.
   s.start_timer();
   for (Uint tuple_size = 0; tuple_size < 32; ++tuple_size) {
     for (int i = 0; i < s.iterations() * 1000; ++i) {
@@ -47,12 +48,13 @@ PICOBENCH(b<latereturn_flatmap_get>).iterations({1,  2,  3,  4,  5,  6,  7,
                                                  15, 16, 17, 18, 19, 20, 21,
                                                  22, 23, 24, 25, 26, 27, 28,
                                                  29, 30, 31, 32});
-PICOBENCH(b<vectoradd_flatmap_get>).iterations({1,  2,  3,  4,  5,  6,  7,
-                                                8,  9,  10, 11, 12, 13, 14,
-                                                15, 16, 17, 18, 19, 20, 21,
-                                                22, 23, 24, 25, 26, 27, 28,
-                                                29, 30, 31, 32});
-PICOBENCH(b<vectoror_flatmap_get>).iterations({1,  2,  3,  4,  5,  6,  7,  8,
-                                               9,  10, 11, 12, 13, 14, 15, 16,
-                                               17, 18, 19, 20, 21, 22, 23, 24,
-                                               25, 26, 27, 28, 29, 30, 31, 32});
+// PICOBENCH(b<vectoradd_flatmap_get>).iterations({1,  2,  3,  4,  5,  6,  7,
+//                                                 8,  9,  10, 11, 12, 13, 14,
+//                                                 15, 16, 17, 18, 19, 20, 21,
+//                                                 22, 23, 24, 25, 26, 27, 28,
+//                                                 29, 30, 31, 32});
+// PICOBENCH(b<vectoror_flatmap_get>).iterations({1,  2,  3,  4,  5,  6,  7,  8,
+//                                                9,  10, 11, 12, 13, 14, 15,
+//                                                16, 17, 18, 19, 20, 21, 22,
+//                                                23, 24, 25, 26, 27, 28, 29,
+//                                                30, 31, 32});
